caeec0a5c5c627cf4a68040f01d9a51f1a5fc406
Sergey Kalentev <skalentev@gmail.com>
2023-06-29 22:07:51 +0300

    add webpack-dev-server end environments

--- README.md
+++ README.md
@@ -3,4 +3,8 @@ Set-ExecutionPolicy Bypass
 npm i -D html-webpack-plugin
 npm i -D typescript ts-loader
 npm install --save-dev typescript ts-node @types/node @types/webpack
+npm i -D webpack-dev-server
 ```
+
+
+https://webpack.js.org/configuration/configuration-languages/
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,6 +1,3 @@
-// const path = require('path');
-// const HTMLWebpackPlugin = require('html-webpack-plugin');
-// const webpack = require('webpack')
 import path from 'path';
 import webpack from 'webpack';
 import HTMLWebpackPlugin from 'html-webpack-plugin';
@@ -8,18 +5,24 @@ import {buildPlugins} from "./config/build/buildPlugins";
 import {buildLoaders} from "./config/build/buildLoaders";
 import {buildResolvers} from "./config/build/buildResolvers";
 import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
-import {BuildPaths} from "./config/build/types/config";
-const paths: BuildPaths = {
-    entry: path.resolve(__dirname, 'src','index.ts'),
-    build: path.resolve(__dirname,'build'),
-    html: path.resolve(__dirname,'public','index.html'),
-}
-const mode = 'development';
-const isDev = mode === 'development';
-const config: webpack.Configuration = buildWebpackConfig({
-    mode: "development",
-    paths,
-    isDev
-})
+import {BuildEnv, BuildPaths} from "./config/build/types/config";
 
-export default config;
+
+export default (env: BuildEnv) =>{
+    const paths: BuildPaths = {
+        entry: path.resolve(__dirname, 'src','index.ts'),
+        build: path.resolve(__dirname,'build'),
+        html: path.resolve(__dirname,'public','index.html'),
+    }
+    const mode = env.mode || 'development';
+    const isDev = mode === 'development';
+    const PORT = env.port || 3000;
+    const config: webpack.Configuration = buildWebpackConfig({
+        mode: mode,
+        paths,
+        isDev,
+        port: PORT,
+    })
+
+    return config;
+}
