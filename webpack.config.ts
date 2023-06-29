3abe6fc2cceeace4f2e6e7123ca0d9367f9eb0af
Sergey Kalentev <skalentev@gmail.com>
2023-06-29 18:36:49 +0300

    typescript config

---
+++ webpack.config.ts
@@ -0,0 +1,25 @@
+// const path = require('path');
+// const HTMLWebpackPlugin = require('html-webpack-plugin');
+// const webpack = require('webpack')
+import path from 'path';
+import webpack from 'webpack';
+import HTMLWebpackPlugin from 'html-webpack-plugin';
+import {buildPlugins} from "./config/build/buildPlugins";
+import {buildLoaders} from "./config/build/buildLoaders";
+import {buildResolvers} from "./config/build/buildResolvers";
+import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
+import {BuildPaths} from "./config/build/types/config";
+const paths: BuildPaths = {
+    entry: path.resolve(__dirname, 'src','index.ts'),
+    build: path.resolve(__dirname,'build'),
+    html: path.resolve(__dirname,'public','index.html'),
+}
+const mode = 'development';
+const isDev = mode === 'development';
+const config: webpack.Configuration = buildWebpackConfig({
+    mode: "development",
+    paths,
+    isDev
+})
+
+export default config;
