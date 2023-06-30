f46e0d2ad9c375994342619d43cb51b7f24ab98c
Sergey Kalentev <skalentev@gmail.com>
2023-06-30 12:57:03 +0300

    add rect css plugins

--- README.md
+++ README.md
@@ -4,7 +4,11 @@ npm i -D html-webpack-plugin
 npm i -D typescript ts-loader
 npm install --save-dev typescript ts-node @types/node @types/webpack
 npm i -D webpack-dev-server
+npm i react react-dom
+npm i -D @types/react @types/react-dom
+npm install sass-loader sass webpack style-loader css-loader --save-dev
 ```
 
 
 https://webpack.js.org/configuration/configuration-languages/
+https://webpack.js.org/loaders/sass-loader/
--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -6,8 +6,21 @@ export function buildLoaders():webpack.RuleSetRule[] {
         use:'ts-loader',
         exclude: /node_modules/,
     }
-    
+
+    const cssLoaders = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+        ],
+    }
+
     return [
         typescriptLoaders,
+        cssLoaders,
     ]
 }
